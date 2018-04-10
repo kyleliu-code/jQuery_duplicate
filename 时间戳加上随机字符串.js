dec1e9b7642e532a2543e4fbec0d17290ce6aff6
palmer <wwwpalmercom@163.com>
2018-04-10 16:26:28 +0800

    js时间戳加上随机字符串

---
+++ 时间戳加上随机字符串.js
@@ -0,0 +1,11 @@
+function randomChar(l) {
+    var x = "0123456789qwertyuioplkjhgfdsazxcvbnm";
+    var tmp = "";
+    var timestamp = new Date().getTime();
+    for (var i = 0; i < l; i++) {
+        tmp += x.charAt(Math.ceil(Math.random() * 100000000) % x.length);
+    }
+    return timestamp + tmp;
+}
+
+randomChar(10);
